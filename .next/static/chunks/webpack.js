ed27de5d04df122e3b248ac2b7ab63eca28f6574
sheru000 <46420588+sheru000@users.noreply.github.com>
2025-10-24 20:36:23 +0500

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "79610584c4e97b85"; }
+/******/ 		__webpack_require__.h = function() { return "a273429c17e0cd39"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
